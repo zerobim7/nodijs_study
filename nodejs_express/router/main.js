bb56cb9064a77315d3bf648e952bd57729f90f51
zerobim7 <deithk@gmail.com>
2018-11-28 18:29:47 +0900

    nodejs express
    get, post part commit

--- nodejs_express/router/main.js
+++ nodejs_express/router/main.js
@@ -11,12 +11,69 @@ module.exports = function(app)
 */
 
 
-module.exports = (app) => {
+module.exports = (app, fs) => {
+
     app.get('/', (req, res) => {
-        res.render('index.html');
+        res.render('index', {
+            title: "MY HOMEPAGE",
+            length: 5
+        });
     });
 
-    app.get('/about', (req, res) => {
-        res.render('about.html');
+
+    app.get('/list', (req, res) => {
+        fs.readFile( __dirname + "/../data/"+"user.json", 'utf8', (err, data) => {
+            console.log(data);
+            res.end(data);
+        });
     });
+
+
+    app.get('/getUser/:username', (req,res) => {
+        fs.readFile( __dirname + "/../data/user.json", 'utf8', (err, data) => {
+            let users = JSON.parse(data);
+            res.json(users[req.params.username]);
+        });
+    });
+
+
+    app.post('/addUser/:username', (req, res) => {
+
+        let result = { };
+        let username = req.params.username;
+
+        //CHECK REQ VALIDITY
+        if(!req.body["password"] || !req.body["name"]) {
+            result["success"] = 0;
+            result["error"] = "invalid request";
+            res.json(result);
+            return;
+        }
+
+        //LOAD DATA & CHECK DUPLICATION
+        fs.readFile( __dirname + "/../data/user.json", 'utf8', (err, data) => {
+            let users = JSON.parse(data);
+            if(users[username]){
+
+                //DUPLICATION FOUND
+                result["success"] = 0;
+                result["error"] = "duplicate";
+                res.json(result);
+                return;
+            }
+
+            //ADD TO DATA
+            users[username] = req.body;
+
+            //SAVE DATA , stringify mean JSON's pretty-print
+            fs.writeFile( __dirname + "/../data/user.json", JSON.stringify(users, null, '\t'), "utf8", (err,data) => {
+                result = {"success": 1};
+                res.json(result);
+            });
+        });
+    });
+
+    
+
+
 }
--- nodejs_express/server.js
+++ nodejs_express/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-let router = require('./router/main')(app); // 라우터모듈인 main.js를 불러와서 app에 전달
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const fs = require('fs');
 
 app.set('views', __dirname + '/views'); //서버가 읽을수 있도록 HTML의 위치를 정의
 app.set('view engine', 'ejs'); //서버가 HTML을 렌더링 할 때, EJS엔진을 사용하다록 설정
@@ -12,6 +14,16 @@ let servser = app.listen(3000, function(){
 
 app.use(express.static('public'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+let router = require('./router/main')(app, fs); // 라우터모듈인 main.js를 불러와서 app에 전달
 //임시 router
 /*
 app.get('/', (req, res) => {
